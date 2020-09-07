8627928568d3080b12b2f1ba3539b0da736055a0
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-07 05:00:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+function saturdayFun(act="roller-skate"){
+  return `This Saturday, I want to ${act}!`;
+}
+
+function mondayWork(act="go to the office"){
+  return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(str="*") {
+  return function(par="special") {
+    return `You are ${str}${par}${str}!`;
+    }
+}
+
+const Calculator={
+  add: function add(){
+    return 1 + 3
+  },
+  subtract: function subtract(){
+    return 1 - 3
+  },
+   multiply: function multiply(){
+    return 1 * 3
+  },
+   divide: function divide(){
+    return 10 / 5
+  },
+  actionApplyer: function actionApplyer(){
+    
+  }
+  
+}
